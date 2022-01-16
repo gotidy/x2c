77fc546d50bc58ad2d98b172ce42148d6f80db30
Evgeny <ya.zver@gmail.com>
2022-01-16 23:52:58 +0700

    Init

---
+++ cmd/xlsx2csv/data.go
@@ -0,0 +1,17 @@
+package main
+
+const (
+	description = `
+Convert XLSX file to CSV.
+
+Copyright (C) 2022 Evgeny Safonov.
+Apache License Version 2.0, January 2004
+http://www.apache.org/licenses/.
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+
+Originally written by Evgeny Safonov.
+Please send bug reports and questions to 
+https://github.com/gotidy/xlsx2csv/issues.
+	`
+)
--- cmd/xlsx2csv/main.go
+++ cmd/xlsx2csv/main.go
@@ -12,7 +12,11 @@ func main() {
 	output := zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}
 	log := zerolog.New(output).With().Timestamp().Logger()
 
-	ctx := kong.Parse(&cli)
+	ctx := kong.Parse(&cli,
+		kong.Name("xlsx2csv"),
+		kong.Description(description),
+		// kong.DefaultEnvars("X2C"),
+	)
 	ctx.Stdout = log
 	err := ctx.Run(&Context{Logger: log})
 	ctx.FatalIfErrorf(err)
